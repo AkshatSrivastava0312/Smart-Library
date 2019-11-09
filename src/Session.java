8d61077aef5995339802919774897e4aa287be2a
AkshatSrivastava0312 <akshat.srivastava0312@gmail.com>
2019-11-09 23:40:32 +0530

    This is a smart Library using JAVA

---
+++ src/Session.java
@@ -0,0 +1,17 @@
+
+import java.util.Date;
+
+public class Session 
+{
+    static String name;
+    static String email;
+    static String id;
+     static String password;
+     static String bookid;
+     static String bookname;
+     static String author;
+     static String category;
+     static int quantity;
+     static Date cur;
+     static Date ret;
+}
